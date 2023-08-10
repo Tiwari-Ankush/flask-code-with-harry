87003fffcba7d5bce5f52aa538385e56f0c94585
Ankush <ankushgt25@gmail.com>
2023-08-10 20:42:19 +0530

    ankushtiwari

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Ankush!'
 
+@app.route('/about')
+def about():
+    return 'Ankush Tiwari\nemail: ankushtiwari@gmail.com'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
