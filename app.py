d9cc583a76772acbb58c0d758a9bddf68556be1a
Ankush <ankushgt25@gmail.com>
2023-08-10 20:48:24 +0530

    ankushtiwari

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Ankush!'
+    return render_template('index.html')
+    # return 'Hello, Ankush!'
 
 @app.route('/about')
 def about():
